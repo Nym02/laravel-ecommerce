d2f32fd8fa12a592a250f5e5931739494f54573d
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-11-12 03:25:55 +0600

    product crud

--- ecom/app/Http/Controllers/Backend/ProductController.php
+++ ecom/app/Http/Controllers/Backend/ProductController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-
 use App\Models\Backend\Product;
 use App\Models\Backend\ProductImage;
+use App\Models\Backend\Category;
+use App\Models\Backend\brands;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Image;
+use File;
 
 class ProductController extends Controller
 {
@@ -28,24 +32,63 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $productCategory = Category::orderBy('cat_name', 'asc')->where('parent_id', 0)->get();
+        $productSubCategory = Category:: orderBy('cat_name', 'asc')->where('parent_id', 'id')->get();
+        $productBrand = brands::orderBy('name', 'asc')->get();
+        return view('Backend.pages.products.create', compact('productCategory', 'productSubCategory', 'productBrand'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+
+        $request->validate([
+            'productTitle' => 'required | max:255',
+            'productRegularPrice' => 'required'
+        ], [
+                'productTitle.required' => 'Please provide product title',
+                'productRegularPrice.required' => 'Please provide product price',
+            ]);
+
+        $product = new Product();
+
+        $product->product_title = $request->productTitle;
+        $product->product_description = $request->productDescription;
+        $product->product_slug = Str::slug($request->productTitle);
+        $product->product_brand_id = $request->productBrand;
+        $product->product_category_id = $request->productCategory;
+        $product->quantity = $request->productQuantity;
+        $product->product_price = $request->productRegularPrice;
+        $product->product_offer_price = $request->productOfferPrice;
+        $product->product_status = $request->productStatus;
+        $product->save();
+
+        if (count($request->productThumbnail) > 0) {
+            foreach ($request->productThumbnail as $productImg) {
+                $img = uniqid() . "." . $productImg->getClientOriginalExtension();
+                $location = public_path('Backend/img/products/' . $img);
+                Image::make($productImg)->save($location);
+
+                $p_img = new ProductImage();
+                $p_img->product_id = $product->id;
+                $p_img->product_image = $img;
+                $p_img->save();
+
+            }
+        }
+        return redirect()->route('product.manage');
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -56,7 +99,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -67,8 +110,8 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -79,7 +122,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- ecom/resources/views/Backend/pages/products/create.blade.php
+++ ecom/resources/views/Backend/pages/products/create.blade.php
@@ -9,12 +9,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Create New Category</h1>
+                        <h1 class="m-0">Create New Product</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Create New Category</li>
+                            <li class="breadcrumb-item active">Create New Product</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -30,7 +30,7 @@
                     <div class="col-12 col-sm-12 col-md-12">
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h3 class="card-title">All Categories</h3>
+                                <h3 class="card-title">Add Product</h3>
 
                                 <div class="card-tools">
                                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -42,32 +42,122 @@
                             <!-- /.card-header -->
 
                             <div class="card-body" style="display: block;">
-                                <form action="{{ route('category.store') }}" method="post" enctype="multipart/form-data">
+                                <form action="{{ route('product.store') }}" method="post" enctype="multipart/form-data">
                                     @csrf
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Category Name</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" name="categoryName" aria-describedby="emailHelp" placeholder="Enter Category Name">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="">Select Parent Category</label>
-                                        <select name="categoryParent" id="" class="form-control">
-                                            <option value="0">Select a category if any</option>
-                                            @foreach($primary_category as $category)
-                                                <option value="{{ $category->id }}">{{ $category->cat_name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="">Category Description</label>
-                                        <textarea name="categoryDesc" id="" cols="30" rows="10" class="form-control" placeholder="Enter Category Description"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleFormControlFile1">Category Logo</label>
-                                        <input type="file" class="form-control-file" name="categoryLogo" id="exampleFormControlFile1">
-                                    </div>
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-xl-6 col-sm-12 col-12">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Title</label>
+                                                <input type="text" class="form-control" name="productTitle"
+                                                       placeholder="Enter Product Name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Description</label>
+                                                <textarea name="productDescription" class="form-control"
+                                                          placeholder="Product Description" id="" cols="30"
+                                                          rows="10"></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Regular Price</label>
+                                                <input type="text" class="form-control" id=""
+                                                       name="productRegularPrice" aria-describedby="emailHelp"
+                                                       placeholder="Regular Price">
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="">Category</label>
+                                                <select name="productCategory" id="" class="form-control">
+                                                    <option value="0">Select a Category</option>
+                                                    @foreach($productCategory as $parentCategory)
+                                                        <option
+                                                            value="{{ $parentCategory->id }}">{{ $parentCategory->cat_name }}</option>
+                                                        @foreach(App\Models\Backend\Category::orderBy('cat_name','asc')->where('parent_id', $parentCategory->id)->get() as $childCategory)
+                                                            <option value="{{ $childCategory->id }}">
+                                                                -- {{ $childCategory->cat_name }}</option>
+                                                        @endforeach
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Brand</label>
+                                                <select name="productBrand" id="" class="form-control">
+                                                    <option value="0">Select a Brand</option>
+                                                    @foreach($productBrand as $brand)
+                                                        <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                                                    @endforeach
+
+                                                </select>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-lg-6 col-md-6 col-xl-6 col-sm-12 col-12">
+                                            <div class="form-group">
+                                                <label for="">Offer Price</label>
+                                                <input type="text" class="form-control" id="exampleInputEmail1"
+                                                       name="productOfferPrice" aria-describedby="emailHelp"
+                                                       placeholder="Offer Price">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Quantity</label>
+                                                <input type="text" class="form-control" id="exampleInputEmail1"
+                                                       name="productQuantity" aria-describedby="emailHelp"
+                                                       placeholder="Quantity">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Status</label>
+                                                <select name="productStatus" id="" class="form-control">
+                                                    <option value="#">Select a Status</option>
+                                                    <option value="1">Active</option>
+                                                    <option value="0">Draft</option>
+
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Product Main Thumbnail</label>
+                                                <input type="file" class="form-control-file" name="productThumbnail[]"
+                                                       id="exampleFormControlFile1">
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                                    <div class="form-group">
+                                                        <label for="">Image 2</label>
+                                                        <input type="file" class="form-control-file"
+                                                               name="productThumbnail[]"
+                                                               id="exampleFormControlFile1">
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                                    <div class="form-group">
+                                                        <label for="">Image 3</label>
+                                                        <input type="file" class="form-control-file"
+                                                               name="productThumbnail[]"
+                                                               id="exampleFormControlFile1">
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                                    <div class="form-group">
+                                                        <label for="">Image 4</label>
+                                                        <input type="file" class="form-control-file"
+                                                               name="productThumbnail[]"
+                                                               id="exampleFormControlFile1">
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                                    <div class="form-group">
+                                                        <label for="">Image 5</label>
+                                                        <input type="file" class="form-control-file"
+                                                               name="productThumbnail[]"
+                                                               id="exampleFormControlFile1">
+                                                    </div>
+                                                </div>
+                                            </div>
 
 
-                                    <button type="submit" class="btn btn-primary d-inline-block">Add New Category</button>
+                                            <button type="submit" class="btn btn-primary d-inline-block">Add New Product
+                                            </button>
+                                        </div>
+                                    </div>
+
                                 </form>
                             </div>
                             <!-- /.card-body -->
