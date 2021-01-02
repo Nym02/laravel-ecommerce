fe378361a026f44a0e494a5e4013765b4d37b691
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-01-03 01:12:37 +0600

    working on authentication system

--- ecom/resources/views/Frontend/includes/topbar.blade.php
+++ ecom/resources/views/Frontend/includes/topbar.blade.php
@@ -1,37 +1,52 @@
 <div class="top-bar animate-dropdown">
     <div class="container">
         <div class="header-top-inner">
-            <div class="cnt-account">
-                <ul class="list-unstyled">
-                    <li><a href="#"><i class="icon fa fa-user"></i>My Account</a></li>
-                    <li><a href="#"><i class="icon fa fa-heart"></i>Wishlist</a></li>
-                    <li><a href="#"><i class="icon fa fa-shopping-cart"></i>My Cart</a></li>
-                    <li><a href="#"><i class="icon fa fa-check"></i>Checkout</a></li>
-                    <li><a href="#"><i class="icon fa fa-lock"></i>Login</a></li>
-                </ul>
-            </div>
-            <!-- /.cnt-account -->
+
 
             <div class="cnt-block">
                 <ul class="list-unstyled list-inline">
-                    <li class="dropdown dropdown-small"> <a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown"><span class="value">USD </span><b class="caret"></b></a>
+                    <li class="dropdown dropdown-small"> <a href="#" class="dropdown-toggle" data-hover="dropdown"
+                            data-toggle="dropdown"><span class="value">USD </span><b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li><a href="#">USD</a></li>
                             <li><a href="#">INR</a></li>
                             <li><a href="#">GBP</a></li>
                         </ul>
                     </li>
-                    <li class="dropdown dropdown-small"> <a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown"><span class="value">English </span><b class="caret"></b></a>
+
+                    @if(Route::has('login'))
+                    @if(Auth::user()->userType === 'USER')
+                    <li class="dropdown dropdown-small"> <a href="#" class="dropdown-toggle" data-hover="dropdown"
+                            data-toggle="dropdown"><span class="value">Welcome, {{ Auth::user()->name }} </span><b
+                                class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li><a href="#">English</a></li>
                             <li><a href="#">French</a></li>
                             <li><a href="#">German</a></li>
                         </ul>
                     </li>
+                    @elseif(Auth::user()->userType !== 'USER' || Auth::user()->userType !== 'ADMIN')
+                    <li><a href="{{ route('login') }}"><i class="icon fa fa-lock"></i>Login</a></li>
+                    <li><a href="{{ route('register') }}"><i class="icon fa fa-lock"></i>Register</a></li>
+                    @endif
+
+                    @endif
+
+
                 </ul>
                 <!-- /.list-unstyled -->
             </div>
             <!-- /.cnt-cart -->
+            <div class="cnt-account">
+                <ul class="list-unstyled">
+                    <li><a href="#"><i class="icon fa fa-user"></i>My Account</a></li>
+                    <li><a href="#"><i class="icon fa fa-heart"></i>Wishlist</a></li>
+                    <li><a href="#"><i class="icon fa fa-shopping-cart"></i>My Cart</a></li>
+                    <li><a href="#"><i class="icon fa fa-check"></i>Checkout</a></li>
+
+                </ul>
+            </div>
+            <!-- /.cnt-account -->
             <div class="clearfix"></div>
         </div>
         <!-- /.header-top-inner -->
